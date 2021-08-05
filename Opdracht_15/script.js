ef6622c89b3b0a96ee62df79b849e6aa74a38ab8
Rein0118 <86830738+Rein0118@users.noreply.github.com>
2021-08-05 14:42:42 +0200

    Add files via upload
    Opdracht_15

---
+++ Opdracht_15/script.js
@@ -0,0 +1,28 @@
+const makePizza = function (){
+    console.log("Kneed het deeg");
+    console.log("Laat het deeg in de koelkast 30 min rusten");
+    console.log("Verwarm de oven op 180 graden")
+    console.log("Maak de saus")
+    console.log("Rol de deeg ter grootte van de pizzavorm")
+    console.log("Bak de bodem 20 min gaar en laat afkoelen")
+    console.log("Smeer saus over de bodem en strooi de kaas erover");
+    console.log("Beleg de pizza met de ingredienten naar keuze");
+    console.log("Bak de pizza in 30 minuten af in de oven");
+    }
+
+    const makeSushi = function(){
+        console.log("Kook de rijst en laat afkoelen");
+        console.log("Pak een zeewiervel");
+        console.log("verdeel de rijst gelijkmatig over het vel");
+        console.log("leg in het midden de ingredienten");
+        console.log("rol het vel op");
+        console.log("snij de rol in gelijke delen");
+    }
+
+    makeSushi()
+    makeSushi()
+    makeSushi()
+    makePizza()
+    makePizza()
+        
+    
