4b53d3f5c15e1d493c17fd15513c162fa39281e6
Rein0118 <86830738+Rein0118@users.noreply.github.com>
2021-08-05 10:28:30 +0200

    Add files via upload
    Opdracht_12

---
+++ Opdracht_12/script.js
@@ -0,0 +1,20 @@
+const age = 30
+if (age >= 18) {
+    console.log("What is your gender?");
+} else {
+    console.log("Only 18 years and older are allowed");
+}
+
+const isFemale = Boolean
+if (isFemale) {
+    console.log("Have fun!");
+} else {
+    console.log("No men allowed");
+}
+
+const driverStatus = 'bob'
+if (driverStatus) {
+    console.log("Drive safely");
+} else {
+    console.log("Je bent geen bob, je mag niet rijden");
+}
