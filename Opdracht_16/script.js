b91a02b4d39d5c3fee7d23b0425b055570d350b2
Rein0118 <86830738+Rein0118@users.noreply.github.com>
2021-08-06 11:52:38 +0200

    Add files via upload
    Opdracht_16

---
+++ Opdracht_16/script.js
@@ -0,0 +1,9 @@
+const paintColor = "blue"
+const locationWall = "north"
+
+const paintWalls = function(paintJob){
+console.log("The " + locationWall + " wall was painted " + paintColor);
+
+}
+
+paintWalls()
